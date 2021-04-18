8ebbdce543308a91a02403f2274f76e68a027379
zx <37545674+imzxx@users.noreply.github.com>
2021-04-18 06:49:13 +0000

    Update RedisTest.java

--- spring-cloud/redis-demo/src/test/java/com/example/redis/RedisTest.java
+++ spring-cloud/redis-demo/src/test/java/com/example/redis/RedisTest.java
@@ -54,8 +54,6 @@ public class RedisTest {
         }
         System.out.println(redisUtils.hashGet("customer", "name"));
         System.out.println(redisUtils.hashGet("customer", "name").equals("张三"));
-        System.out.println("提交");
-        System.out.println("测试提交冲突");
     }
 
 }
